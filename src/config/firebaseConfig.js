1a946b46f2a55e9cee31e73d1f56b659badf0a82
nkumar007 <naveen.mbuff98@gmail.com>
2023-06-03 20:37:06 +0530

    using firebase in our project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBquNlYkrt7wW-b1Q4pJDvgMP8ydWCm3z0",
+
+  authDomain: "crypto-tracker-4d9d0.firebaseapp.com",
+
+  projectId: "crypto-tracker-4d9d0",
+
+  storageBucket: "crypto-tracker-4d9d0.appspot.com",
+
+  messagingSenderId: "542229409905",
+
+  appId: "1:542229409905:web:e558a48a0716c3196a2bd0",
+};
+
+export default firebaseConfig;
